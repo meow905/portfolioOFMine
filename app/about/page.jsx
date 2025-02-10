3584dbf68e23c356757897b7b6d05991f283c12a
meow905 <tabrezbunny@gmail.com>
2025-02-10 21:22:51 +0500

    newVersion-1

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,31 +1,45 @@
-"use client"
+"use client";
 
 import React from "react";
 import { useLanguage } from "../hooks/useLanguage";
 
 export default function About() {
+  const context = useLanguage();
+  if (!context) {
+    console.error(
+      "useLanguage должен быть использован внутри LanguageProvider"
+    );
+    return null;
+  }
 
-const { language } = useLanguage();
+  const { language } = context;
 
-const aboutText = {
-  aboutMe: {
-    en: "About me",
-    ru: "Обо мне",
-  },
-  text: {
-    en: "Hi, I&apos;m Tabrez – Frontend developer from Dushanbe.\nI&apos;m interested in programming and everything connected \nwith it.",
-    ru: "Привет, меня зовут Табрез - я фронтенд-разработчик из Душанбе.\n Я интересуюсь программированием \nи всем, что с этим связано.",
-  },
-  text2: {
-    en: "I&apos;m studying at courses Introduction to Frontend in\nIlmhona.",
-    ru: "Я изучаю курсы по фротенд-разработке в Илмхоне",
-  },
-  text3: {
-    en: "Ready to implement excellent projects \n with wonderful people.",
-    ru: "Готов реализовать отличные проекты \n с замечательными людьми.",
-  },
-};
-
+  const aboutText = {
+    aboutMe: {
+      en: "About me",
+      ru: "Обо мне",
+    },
+    text: {
+      en: [
+        "Hi, I'm Tabrez – Frontend developer from Dushanbe.",
+        "I'm interested in programming and everything connected",
+        "with it.",
+      ],
+      ru: [
+        "Привет, меня зовут Табрез - я фронтенд-разработчик из Душанбе.",
+        "Я интересуюсь программированием",
+        "и всем, что с этим связано.",
+      ],
+    },
+    text2: {
+      en: ["I'm studying at courses Introduction to Frontend in Ilmhona."],
+      ru: ["Я изучаю курсы по фротенд-разработке в Илмхоне"],
+    },
+    text3: {
+      en: ["Ready to implement excellent projects", "with wonderful people."],
+      ru: ["Готов реализовать отличные проекты", "с замечательными людьми."],
+    },
+  };
 
   return (
     <div
@@ -33,30 +47,19 @@ const aboutText = {
       className="bg-neutral-100 text-black text-center py-24 xl:px-0 px-14"
     >
       <h1 className="text-4xl font-bold">{aboutText.aboutMe[language]}</h1>
-      <p className="my-5">
-        {aboutText.text[language].split("\n").map((line, index) => (
-          <React.Fragment key={index}>
-            {line}
-            <br />
-          </React.Fragment>
-        ))}
-      </p>
-      <p className="my-5">
-        {aboutText.text2[language].split("\n").map((line, index) => (
-          <React.Fragment key={index}>
-            {line}
-            <br />
-          </React.Fragment>
-        ))}
-      </p>
-      <p>
-        {aboutText.text3[language].split("\n").map((line, index) => (
-          <React.Fragment key={index}>
-            {line}
-            <br />
-          </React.Fragment>
-        ))}
-      </p>
+      {aboutText.text[language].map((line, index) => (
+        <p key={index} className="my-5">
+          {line}
+        </p>
+      ))}
+      {aboutText.text2[language].map((line, index) => (
+        <p key={index} className="my-5">
+          {line}
+        </p>
+      ))}
+      {aboutText.text3[language].map((line, index) => (
+        <p key={index}>{line}</p>
+      ))}
     </div>
   );
 }
